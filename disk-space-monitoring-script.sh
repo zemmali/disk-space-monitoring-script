77faec227038dafe1f7247b9ca8d8c805d51625d
saddam <zemmali@users.noreply.github.com>
2015-12-22 15:21:04 +0100

    Create disk-space-monitoring-script.sh
    SHELL script

---
+++ disk-space-monitoring-script.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# AUTHOR: Saddam ZEMMALI
+# DATE: 22th December 2015
+# Shell script to monitor or watch the low-disk space
+# It will send an email to $ADMIN, if the (free available) percentage
+# of space is >= 90%
+#### Script START here ###
+## You can change your threshold value whatever you want ##
+THRESHOLD=90
+PATHS=/
+AWK=/bin/awk
+DU=`/usr/bin/du -ks`
+GREP=/bin/grep
+SED=/bin/sed
+CAT=/bin/cat
+MAILFILE=/tmp/mailviews$$
+MAILER=/bin/mail
+## Change ADMIN Mail address as per the requirement ##
+mailto=saddam.zemmali@gmail.com
+for path in $PATHS
+do
+## Validate the Percentage of Disk space ##
+DISK_AVAIL=`/bin/df -k / | grep -v Filesystem |awk '{print $5}' |sed 's/%//g'`
+if [ $DISK_AVAIL -ge $THRESHOLD ]
+then
+echo "Please clean up your stuff \n\n" > $MAILFILE
+$CAT $MAILFILE | $MAILER -s "Clean up stuff" $mailto
+fi
+done
+## END of the Script ##
